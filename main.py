f906ec9e715fad00afee89fbf2e09e31167f617b
PriyaM <priyamalaghan31@gmail.com>
2025-06-12 16:24:59 -0400

    data ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==============X")
+#Exception is the base class for all built-in exceptions, so this catches any type of error
+except Exception as e:
+    #Logs the exception with full stack trace, helping in debugging.
+    logger.exception(e)
+    raise e
+    
---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+#This creates a Path object that points to a file called config.yaml located inside the config/ folder
+CONFIG_FILE_PATH = Path("config/config.yaml")
+#This defines another Path object pointing to a file called params.yaml, located in the root directory of the project.
+PARAMS_FILE_PATH = Path("params.yaml")
