9e98471c24e1d5e95615979a1e538c1c22870eb7
PriyaM <priyamalaghan31@gmail.com>
2025-06-20 12:18:45 -0400

    data transformation added

--- main.py
+++ main.py
@@ -4,26 +4,37 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformatn import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==============X")
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nX==============X")
 #Exception is the base class for all built-in exceptions, so this catches any type of error
 except Exception as e:
     #Logs the exception with full stack trace, helping in debugging.
     logger.exception(e)
     raise e
 
-STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Data Validation Stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==============X")
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nX==============X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nX================X")
 except Exception as e:
     logger.exception(e)
     raise e
